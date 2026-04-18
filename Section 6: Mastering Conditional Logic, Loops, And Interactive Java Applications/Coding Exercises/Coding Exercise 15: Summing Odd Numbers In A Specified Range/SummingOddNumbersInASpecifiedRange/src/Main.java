e6a40c2d9ac6c40199971301377938a04925cd94
Juan Carlos Martin Nogueira <juannogueiram@gmail.com>
2026-04-18 15:17:26 -0400

    Add files via upload

---
+++ Section 6: Mastering Conditional Logic, Loops, And Interactive Java Applications/Coding Exercises/Coding Exercise 15: Summing Odd Numbers In A Specified Range/SummingOddNumbersInASpecifiedRange/src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static boolean isOdd(int number){
+        if (number <= 0) return false;
+        return number % 2 != 0;
+    }
+
+    public static int sumOdd(int start, int end){
+        int counter = 0;
+
+        if (start <= 0 || end <= 0 || end < start){
+            return -1;
+        }
+
+        for(int i = start; i <= end; i++){
+            if (isOdd(i)) counter += i;
+        }
+
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOdd(1, 100));
+        System.out.println(sumOdd(-1, 100));
+        System.out.println(sumOdd(100, 100));
+        System.out.println(sumOdd(13, 13));
+        System.out.println(sumOdd(100, 1000));
+    }
+}
