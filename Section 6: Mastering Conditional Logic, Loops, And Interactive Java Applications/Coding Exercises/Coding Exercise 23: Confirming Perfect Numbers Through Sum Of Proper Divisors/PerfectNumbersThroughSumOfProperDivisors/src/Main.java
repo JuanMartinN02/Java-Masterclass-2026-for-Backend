d66be7a66c3f2eb251c479290eb30e5ea3a400e0
Juan Carlos Martin Nogueira <juannogueiram@gmail.com>
2026-04-21 16:14:59 -0400

    Add files via upload

---
+++ Section 6: Mastering Conditional Logic, Loops, And Interactive Java Applications/Coding Exercises/Coding Exercise 23: Confirming Perfect Numbers Through Sum Of Proper Divisors/PerfectNumbersThroughSumOfProperDivisors/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static boolean isPerfectNumber(int number){
+        if(number < 1) return false;
+
+        int sum = 0;
+
+        for(int i = 1; i <= number / 2; i++){
+            if(number % i == 0) sum += i;
+        }
+
+        return (sum == number);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+    }
+}
